3760df2ff83072ba8509abac1991f5c832c258e8
Loading User <scaretime@gmail.com>
2015-04-26 14:16:31 -0400

    New Map

--- src/installs/maps/Lorkeep.py
+++ src/installs/maps/Lorkeep.py
@@ -7,4 +7,15 @@ class Lorkeep(WawMaps):
 	def install(self):
 		print('mapname = ' + self.mapname)
 		print('homepage = ' + self.homepage)
-		print('map_ho')
+		print('map_homepage = ' + self.map_homepage)
+		print('Installing Files...')
+		## subproccess.call([C://Dekstop/nazi_zombie_lorkeep.exe])
+		time.sleep(2)
+		print('Creating Box...')
+		time.sleep(2)
+		print('Downloading Custom Models')
+		time.slep(2)
+		print('Lorkeep Successfuly Installedd')
+
+	def uninstall(self):
+		print 
